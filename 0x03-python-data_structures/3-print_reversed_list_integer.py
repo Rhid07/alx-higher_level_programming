27fc8266a96725b74e6692ec0de4339881c1ae27
Rhid07 <opeyemidurodola007@gmail.com>
2024-01-20 03:55:11 +0100

    task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def new_in_list(my_lists idx, element):
-    new_list = my_lists[:]
-    if 0 <= idx < len(new_list):
-        new_list[idx] = element
-        return (new_list)
-    return (my_lists)
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        for elm in my_list[::-1]:
+            print("{:d}".format(elm))
